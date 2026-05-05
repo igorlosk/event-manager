a736abf4eda9200492181a2ab29c9e0d90c0472b
Igor Loskutnikov <igororder@gmail.com>
2026-05-05 08:20:31 +0300

    searchDto validation fix

--- event-manager/src/main/java/dev/sorokin/eventmanager/event/EventSearchRequestDto.java
+++ event-manager/src/main/java/dev/sorokin/eventmanager/event/EventSearchRequestDto.java
@@ -1,30 +1,15 @@
 package dev.sorokin.eventmanager.event;
 
-
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
 public record EventSearchRequestDto(
-        @NotBlank
         String name,
-        @Min(1)
         Integer placesMin,
-        @Min(1)
         Integer placesMax,
-        @NotBlank
         String dateStartAfter,
-        @NotBlank
         String dateStartBefore,
-        @Min(1)
         Integer costMin,
-        @Min(1)
         Integer costMax,
-        @Min(30)
         Integer durationMin,
-        @Min(30)
         Integer durationMax,
-        @NotNull
         Integer locationId,
         EventStatus eventStatus
 ) {
