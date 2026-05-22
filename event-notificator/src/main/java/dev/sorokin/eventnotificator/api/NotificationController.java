86b72e25daa0af42aef816ae0738f625fca5a76c
igorlosk <igor@losku.ru>
2026-05-22 16:55:31 +0300

    bugs fix

--- event-notificator/src/main/java/dev/sorokin/eventnotificator/api/NotificationController.java
+++ event-notificator/src/main/java/dev/sorokin/eventnotificator/api/NotificationController.java
@@ -1,7 +1,8 @@
 package dev.sorokin.eventnotificator.api;
 
 import dev.sorokin.eventnotificator.domain.NotificationService;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,29 +12,30 @@ import java.util.List;
 @RestController("/notifications")
 public class NotificationController {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(NotificationController.class);
+
     private final NotificationService notificationService;
 
     private final NotificationResponseToDtoMapper notificationResponseToDtoMapper;
 
-    public NotificationController(NotificationService notificationService, NotificationResponseToDtoMapper notificationResponseToDtoMapper) {
+    public NotificationController(
+            NotificationService notificationService,
+            NotificationResponseToDtoMapper notificationResponseToDtoMapper
+    ) {
         this.notificationService = notificationService;
         this.notificationResponseToDtoMapper = notificationResponseToDtoMapper;
+
     }
 
     @GetMapping
     public List<NotificationResponseDto> getNotifications(
             @RequestHeader(value = "Authorization", required = false) String authHeader) {
 
-        String jwtToken = null;
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            jwtToken = authHeader.substring(7);
-        }
-
-        System.out.println("JWT Token: " + jwtToken);
         return notificationService.getAllNotifications()
                 .stream()
                 .map(notificationResponseToDtoMapper::toDto)
                 .toList();
     }
+
 
 }
